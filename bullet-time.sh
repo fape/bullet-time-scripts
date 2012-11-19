c8881c61b59863b875912358c34056a1317e3c08
fape <fape88@gmail.com>
2012-11-19 21:41:04 +0100

    improve null check

--- bullet-time.sh
+++ bullet-time.sh
@@ -41,7 +41,7 @@ do
 		user=`gphoto2 --get-config "${OWNER_CONFIG}" --port "${port}" --camera "${camera}"| sed -ne "s/Current:\(.*\)/\1/p" | sed -e "s/^\s*//g" -e "s/\s*$//g"`
 		
 		#check user name, use generated if no user information
-		if [ "x == x${user}" ];
+		if [ "x" == "x${user}" ];
 		then
 			user=`tr -dc A-Za-z0-9_ < /dev/urandom | head -c8`
 			echo "Use generated name: ${user} on ${camera}, ${port}" 1>&2
