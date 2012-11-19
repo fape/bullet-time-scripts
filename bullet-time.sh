7d2d58bba78171e3119636ce5e94c6513750644a
fape <fape88@gmail.com>
2012-11-19 21:38:18 +0100

    improve null check

--- bullet-time.sh
+++ bullet-time.sh
@@ -32,7 +32,7 @@ do
 		camera=`echo ${line} | sed -e "s/\(.*\)\(${port}\)/\1/g" -e "s/^\s*//g" -e "s/\s*$//g"`
 	
 		#check camera name	
-		if [ x == x${camera} ];
+		if [ "x" == "x${camera}" ];
 		then
 			echo "Unsupported camera on ${port}" 1>&2
 			continue
@@ -41,7 +41,7 @@ do
 		user=`gphoto2 --get-config "${OWNER_CONFIG}" --port "${port}" --camera "${camera}"| sed -ne "s/Current:\(.*\)/\1/p" | sed -e "s/^\s*//g" -e "s/\s*$//g"`
 		
 		#check user name, use generated if no user information
-		if [ x == x${user} ];
+		if [ "x == x${user}" ];
 		then
 			user=`tr -dc A-Za-z0-9_ < /dev/urandom | head -c8`
 			echo "Use generated name: ${user} on ${camera}, ${port}" 1>&2
