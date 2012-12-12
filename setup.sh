43a5ffa1cb69b226e7ec51e2de9f0bcad0227575
spy14 <balint.sipter@gmail.com>
2012-12-12 15:14:44 +0100

    image quality now works

--- setup.sh
+++ setup.sh
@@ -14,5 +14,34 @@ WHITEBALACE="/main/imgsettings/whitebalance"
 SYNCTIME="/main/actions/syncdatetime=1"
 IMGFORMAT="/main/imgsettings/imageformat"
 OWNERNAME="/main/settings/ownername"
+GET="--get-config"
+SET="--set-config"
 
-# 8==D
+OIFS=$IFS # save default value
+IFS=";"
+PS3="Type a number: "
+
+INPUT=`LANG=C gphoto2 $GET $IMGFORMAT` 
+DATA=`echo ${INPUT}| grep "Choice" | sed "s/Choice: \([0-9]\)\+ \(\([a-zA-Z]\)\+\)/\2/g" | tr "\\n" ";"; echo  "Quit" `
+CURRENT=`echo ${INPUT} | grep "Current"`
+OPTIONLENGHT=`echo "$DATA" | grep -o ";" | wc -l`
+
+echo "Choose Image format:"
+echo "$CURRENT"
+
+select value in ${DATA}; do
+        if [ -n "${value}" ]; then
+                echo "${value} selected"
+
+		if [ $REPLY -le $OPTIONLENGHT ]; then
+		((REPLY--))	
+		gphoto2 $SET $IMGFORMAT=$REPLY
+		
+		fi
+		break
+        fi
+
+done
+
+IFS=$OIFS
+
