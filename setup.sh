a28637315b32334ebb41da21780bd160ef22455a
spy14 <balint.sipter@gmail.com>
2012-12-12 02:59:24 +0100

    created setup.readme, done some work  on setup.sh

--- setup.sh
+++ setup.sh
@@ -3,109 +3,16 @@
 ######################################################################
 #                                                                    #
 #  See documentation on https://github.com/fape/bullet-time-scripts  #
-#                                                                    #
+#		Make sure to read the setup.readme                   #
+#								     #
 ######################################################################
-#Iso settings(450D):
-#Choice: 0 Automatic
-#Choice: 1 100
-#Choice: 2 200
-#Choice: 3 400
-#Choice: 4 800
-#Choice: 5 1600
+
 ISO="/main/imgsettings/iso"
-#Aperture settings(450D):
-#Choice: 0 4
-#Choice: 1 4.5
-#Choice: 2 5
-#Choice: 3 5.6
-#Choice: 4 6.3
-#Choice: 5 7.1
-#Choice: 6 8
-#Choice: 7 9
-#Choice: 8 10
-#Choice: 9 11
-#Choice: 10 13
-#Choice: 11 14
-#Choice: 12 16
-#Choice: 13 18
-#Choice: 14 20
-#Choice: 15 22
-#Choice: 16 25
 APERTURE="/main/capturesettings/aperture"
-Choice: 0 bulb
-Choice: 1 30
-Choice: 2 25
-Choice: 3 20
-Choice: 4 15
-Choice: 5 13
-Choice: 6 10
-Choice: 7 8
-Choice: 8 6
-Choice: 9 5
-Choice: 10 4
-Choice: 11 3.2
-Choice: 12 2.5
-Choice: 13 2
-Choice: 14 1.6
-Choice: 15 1.3
-Choice: 16 1
-Choice: 17 0.8
-Choice: 18 0.6
-Choice: 19 0,5
-Choice: 20 0.4
-Choice: 21 0.3
-Choice: 22 1/4
-Choice: 23 1/5
-Choice: 24 1/6
-Choice: 25 1/8
-Choice: 26 1/10
-Choice: 27 1/13
-Choice: 28 1/15
-Choice: 29 1/20
-Choice: 30 1/25
-Choice: 31 1/30
-Choice: 32 1/40
-Choice: 33 1/50
-Choice: 34 1/60
-Choice: 35 1/80
-Choice: 36 1/100
-Choice: 37 1/125
-Choice: 38 1/160
-Choice: 39 1/200
-Choice: 40 1/250
-Choice: 41 1/320
-Choice: 42 1/400
-Choice: 43 1/500
-Choice: 44 1/640
-Choice: 45 1/800
-Choice: 46 1/1000
-Choice: 47 1/1250
-Choice: 48 1/1600
-Choice: 49 1/2000
-Choice: 50 1/2500
-Choice: 51 1/3200
-Choice: 52 1/4000
 SHUTTER_SPEED="/main/capturesettings/shutterspeed"
-Current: Fluoreszkáló
-Choice: 0 Automatikus
-Choice: 1 Daylight
-Choice: 2 Shadow
-Choice: 3 Felhős
-Choice: 4 Tungsten
-Choice: 5 Fluoreszkáló
-Choice: 6 Vaku
-Choice: 7 Manual
 WHITEBALACE="/main/imgsettings/whitebalance"
-#Synchronise time with PC
 SYNCTIME="/main/actions/syncdatetime=1"
-#Image formats(450D):
-#	Choice: 0 Large Fine JPEG
-#	Choice: 1 Large Normal JPEG
-#	Choice: 2 Medium Fine JPEG
-#	Choice: 3 Medium Normal JPEG
-#	Choice: 4 Small Fine JPEG
-#	Choice: 5 Small Normal JPEG
-#	Choice: 6 RAW + Large Fine JPEG
-#	Choice: 7 RAW
 IMGFORMAT="/main/imgsettings/imageformat"
 OWNERNAME="/main/settings/ownername"
+
+# 8==D
