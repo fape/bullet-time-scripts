692e57404b92ce46e0433f91d68d0adc3acc00e8
spy14 <balint.sipter@gmail.com>
2012-12-12 01:59:33 +0100

    added setup

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+######################################################################
+#                                                                    #
+#  See documentation on https://github.com/fape/bullet-time-scripts  #
+#                                                                    #
+######################################################################
+ISO=""
+APERTURE=""
+SHUTTER_SPEED=""
+WHITE_BALACE=""
+AUTO_POWER=""
+EV_COMPENSATION=""
+QUALITY=""
