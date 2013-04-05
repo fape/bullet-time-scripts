07a23363a56b53b559ac8725fda3693ac1ccb7cb
fape <fape88@gmail.com>
2013-04-05 22:59:56 +0200

    change deps download method

--- install_debian.sh
+++ install_debian.sh
@@ -3,6 +3,8 @@
 mkdir deps
 cd deps
 
+$CPU=`grep -c processor /proc/cpuinfo`
+
 #remove old packages
 sudo apt-get remove gphoto2 libgphoto2-2 libgphoto2-l10n libgphoto2-port0 jhead
 
@@ -10,21 +12,31 @@ sudo apt-get remove gphoto2 libgphoto2-2 libgphoto2-l10n libgphoto2-port0 jhead
 sudo apt-get install build-essential libtool libusb-1.0-0-dev libusb-dev libjpeg-dev libexif-dev libpopt-dev liblockdev1-dev libreadline-dev libcdk5-dev libaa1-dev libgd2-xpm-dev git
 
 #download sources
-wget "http://sourceforge.net/projects/gphoto/files/libgphoto/2.5.1.1/libgphoto2-2.5.1.1.tar.bz2/download" -O libgphoto2-2.5.1.1.tar.gz
-wget "http://sourceforge.net/projects/gphoto/files/gphoto/2.5.1/gphoto2-2.5.1.tar.gz/download" -O gphoto2-2.5.1.tar.gz
+#wget "http://sourceforge.net/projects/gphoto/files/libgphoto/2.5.1.1/libgphoto2-2.5.1.1.tar.bz2/download" -O libgphoto2-2.5.1.1.tar.gz
+git clone https://github.com/fape/libgphoto2.git
+cd libgphoto2
+git submodule update --init
+cd ..
+git clone https://github.com/fape/gphoto2.git
+cd gphoto2
+git submodule update --init
+cd ..
+#wget "http://sourceforge.net/projects/gphoto/files/gphoto/2.5.1/gphoto2-2.5.1.tar.gz/download" -O gphoto2-2.5.1.tar.gz
 wget "http://www.sentex.net/~mwandel/jhead/jhead-2.97.tar.gz"
 
 #extract
 echo *.tar.gz | xargs -n 1 tar -xvf
 
 #install libgphoto2
-cd libgphoto2-2.5.1.1
+cd libgphoto2
+autoreconf -is
 ./configure --prefix=/usr
-make -j9
+make -j $CPU
 sudo make install
 
 #install gphoto2
-cd ../gphoto2-2.5.1
+cd ../gphoto2
+autoreconf -is
 ./configure --with-aalib
 make
 sudo make install
